ea21356635b31247ea75d071df8aa4702978e570
Orion Gael <orion.gael@gmail.com>
2026-04-14 22:44:38 -0700

    Fix output image resolution

--- server/update-display.py
+++ server/update-display.py
@@ -6,5 +6,6 @@ image_path = sys.argv[1]  # passed in from Node
 
 inky_display = auto()
 img = Image.open(image_path)
+img = img.rotate(90, expand=True)
 inky_display.set_image(img)
 inky_display.show()
